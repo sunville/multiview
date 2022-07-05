8bc8439ec261ffeb80c61114a829c86f5a88041f
cswuhao <1155076729@link.cuhk.edu.hk>
2022-07-05 23:56:50 +0800

    ta

--- py/multiview_sphere.py
+++ py/multiview_sphere.py
@@ -2,16 +2,17 @@ import numpy
 import numpy as np
 from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 # pos_p = neg_p = 0.5, alpha = 2
 # we test 2-d case
 
-dimension = 2
+dimension = 3
 pos_p = 0.5
 neg_p = 1 - pos_p
 sample_N = 100000
-H_1_size = 100  # divide 2pi
-H_2_size = 100  # divide 2pi
+H_1_delta = 100  # divide 2pi
+H_2_delta = 100  # divide 2pi
 eps = 0.01
 delta = 0.01
 constant_labels = 10
@@ -19,8 +20,20 @@ constant_labels = 10
 random_seed = 1
 round_point_approx = 0.000001
 
+def cartesian_product_transpose(arrays):
+    broadcastable = numpy.ix_(*arrays)
+    broadcasted = numpy.broadcast_arrays(*broadcastable)
+    rows, cols = numpy.prod(broadcasted[0].shape), len(broadcasted)
+    dtype = numpy.result_type(*arrays)
+    out = numpy.empty(rows * cols, dtype=dtype)
+    start, end = 0, rows
+    for a in broadcasted:
+        out[start:end] = a.reshape(-1)
+        start, end = end, end + rows
+    return out.reshape(cols, rows).T
 
-def generate_H_v(H_v_size):
+
+def generate_H_v(H_v_delta):
     '''
     :param H_v_size: size of hypothesis set H_v
     :return: hypothesis set H_v
@@ -29,33 +42,44 @@ def generate_H_v(H_v_size):
     H_i : numpy array of size: H_i_size * dimension
     '''
 
-    H_v = numpy.ones(shape=(H_v_size, dimension))
-    H_v_polar = numpy.zeros(shape=(H_v_size, dimension - 1))
+    H_v = numpy.ones(shape=(H_v_delta**(dimension-1), dimension))
+    H_v_polar = numpy.zeros(shape=(H_v_delta, dimension - 1))
+
     # Generate hypothesis by rotating unit vector in polar coordinates
     for i in range(dimension - 2):
-        H_v_polar[:,i] = np.linspace(0.0, np.pi, H_v_size)
+        H_v_polar[:, i] = np.linspace(0.0, np.pi, H_v_delta)
 
-    H_v_polar[:,dimension-1] = np.linspace(0.0, 2*np.pi, H_v_size)
+    H_v_polar[:, -1] = np.linspace(0.0, 2*np.pi, H_v_delta)
+    H_v_polar = np.array([H_v_polar[:,i] for i in range(dimension - 1)])
 
-    H_v[:,0] = np.cos(H_v_polar[:,0])
-    sin_polar = np.ones(shape=(H_v_size, 1))
+    H_v_polar = cartesian_product_transpose(H_v_polar)
+
+    H_v[:, 0] = np.cos(H_v_polar[:, 0])
+    sin_polar = np.sin(H_v_polar[:, 0])
+
     for i in range(1, dimension-1):
-        sin_polar *= np.sin(H_v_polar[:,i-1])
-        H_v[:,i] = sin_polar * np.cos(H_v_polar[:,i])
+        H_v[:, i] = sin_polar * np.cos(H_v_polar[:, i])
+        sin_polar *= np.sin(H_v_polar[:, i])
 
-    H_v[:,-1] = sin_polar * np.sin(H_v_polar[:,-1])
+    H_v[:, -1] = sin_polar
 
-    del H_v_polar
+    # del H_v_polar
 
+    ax = plt.axes(projection='3d')
+    xline = H_v[:, 0]
+    yline = H_v[:, 1]
+    zline = H_v[:, 2]
+    ax.scatter3D(xline,yline,zline, cmap='Greens')
+    # plt.plot(xline, yline, 'bo')
+    plt.show()
     return H_v
 
 
 def generate_hypotheses():
-    H_1 = generate_H_v(H_1_size)
-    H_2 = generate_H_v(H_2_size)
+    H_1 = generate_H_v(H_1_delta)
+    # H_2 = generate_H_v(H_2_delta)
 
-    # Generate vectors by polar coordinates
-    plt.plot()
+    # Generate vectors by polar coordinateS
 
 
 def choose_concept(H):
